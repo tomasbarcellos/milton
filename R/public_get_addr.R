3bccc19d304e4036265046f31fdf05d47057d4a5
Tomás de Siervi Barcellos <tomasbarcellos@gmail.com>
2022-01-20 01:16:27 -0300

    Remove chamadas de require de dentro da função (má-prática)

--- R/public_get_addr.R
+++ R/public_get_addr.R
@@ -1,24 +1,24 @@
 ##' get_addr
 #'
 #' Returns the corresponding geocoordinates (latitude, longitude) of a given an address .
-#' Uses Open Street Maps JSON API (requires jsonlite). By D.Kisler 
+#' Uses Open Street Maps JSON API (requires jsonlite). By D.Kisler
 #'
-#' @param address Character value containing street adress 
+#' @param address Character value containing street adress
 #' @export
-#' 
+#'
 
 get_addr <- function(address = NULL){
-  require(jsonlite)
-  require(curl)
-  
+
   if(suppressWarnings(is.null(address)))
     return(data.frame())
+
   tryCatch(
     d <- jsonlite::fromJSON(
-      gsub('\\@addr\\@', gsub('\\s+', '\\%20', address), 
+      gsub('\\@addr\\@', gsub('\\s+', '\\%20', address),
            'http://nominatim.openstreetmap.org/search/@addr@?format=json&addressdetails=0&limit=1')
     ), error = function(c) return(data.frame())
   )
   if(length(d) == 0) return(data.frame())
+
   return(data.frame(lon = as.numeric(d$lon), lat = as.numeric(d$lat)))
 }
