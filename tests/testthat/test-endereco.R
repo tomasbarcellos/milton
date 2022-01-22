a330b1474d4dc05ee549ec77330f5b03f062dd7b
Tomás de Siervi Barcellos <tomasbarcellos@gmail.com>
2022-01-22 10:27:56 -0300

    Inclclui CEP da documentação, #1

--- tests/testthat/test-endereco.R
+++ tests/testthat/test-endereco.R
@@ -42,4 +42,8 @@ test_that("Retorna lat long de CEP", {
                ))
 
   expect_equal(get_addr(88063000), get_addr("Avenida pequeno principe"))
+
+  expect_equal(milton:::cep(70050000), "Esplanada Dos Ministérios")
+  expect_equal(milton:::cep(68915970), "Avenida Costa e Silva")
+
 })
