ab220686ac5d4d50f01fcbebab1ce2e7b6039f08
Tomás de Siervi Barcellos <tomasbarcellos@gmail.com>
2022-01-23 00:01:00 -0300

    Inclui forma de identificar o setor censitario de SP

--- tests/testthat/test-parte.R
+++ tests/testthat/test-parte.R
@@ -10,6 +10,9 @@ X <- rbind(x, y, z, w)
 regioes <- readRDS("regioes.rds")
 mun <- readRDS("mun.rds")
 
+cens <- cens3 <- sf::st_read("inst/extdata/setor censitario", "DEINFO_SETOR_CENSITARIO_2010") %>%
+  sf::st_transform("+proj=longlat +zone=23 +south +ellps=aust_SA +units=m +no_defs") %>%
+  sf::st_as_sf()
 
 test_that("Identificar ponto em poligono", {
   # Cidades estao nas regioes corretas
@@ -24,4 +27,12 @@ test_that("Identificar ponto em poligono", {
   coord2 <- get_addr("Rua vergueiro")
   res3 <- geopart(coord2, mun)
   expect_equal(as.character(res3$nome), "SÃO PAULO")
+
+  res4 <- geopart(coord, cens)
+  # Fora de SP
+  expect_equal(nrow(res4), 0L)
+
+  res5 <- geopart(coord2, cens)
+  expect_equal(res5$ID, 14465)
 })
+
