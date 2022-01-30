47d405e6faad6b6a43b48d2478a4d445206c6d7b
Tomás de Siervi Barcellos <tomasbarcellos@gmail.com>
2022-01-30 08:34:51 -0300

    Remove arquivos de ext data. Inclui função para ler dados IPVS

---
+++ R/ipvs.R
@@ -0,0 +1,27 @@
+#' Ler dados do Indice Paulista de Vulnerabilidade Social (IPVS)
+#'
+#' @return Uma tibble
+#' @export
+ler_ipvs <- function() {
+  td <- tempdir()
+  tf <- tempfile(tmpdir = td)
+  httr::GET("http://www.ipvs.seade.gov.br/view/zip/ipvs/BaseIPVS2010_csv.zip",
+            httr::write_disk(tf))
+  unzip(tf, exdir = td)
+  dicionario <- readr::read_tsv(file.path(td, "IPVS_Dicionario.txt"),
+                                locale = readr::locale(encoding = "UCS-2LE")) %>%
+    janitor::clean_names() %>%
+    head(51) %>%
+    dplyr::mutate(
+      descricao = descricao %>%
+        stringr::str_remove("\\(.+\\)") %>%
+        stringr::str_squish()
+    ) %>%
+    unique()
+  ipvs <- readr::read_csv2(file.path(td, "BaseIPVS2010.csv")) %>%
+    purrr::set_names(dicionario$descricao) %>%
+    janitor::clean_names()
+
+  ipvs %>%
+    dplyr::mutate(codigo_do_setor_censitario = as.character(codigo_do_setor_censitario ))
+}
