3c98b81160f908c3429cd076e950caf75f695092
Tomás de Siervi Barcellos <tomasbarcellos@gmail.com>
2022-02-05 18:27:54 -0300

    separa testes de CEP do endereço

--- tests/testthat/test-endereco.R
+++ tests/testthat/test-endereco.R
@@ -29,10 +29,3 @@ test_that("Erros sao suaves", {
 
 })
 
-test_that("Retorna lat long de CEP", {
-  expect_equal(get_addr(88063000), get_addr("Avenida pequeno principe"))
-
-  expect_equal(milton:::cep(70050000), "Esplanada Dos Ministérios")
-  expect_equal(milton:::cep(68915970), "Avenida Costa e Silva")
-
-})
