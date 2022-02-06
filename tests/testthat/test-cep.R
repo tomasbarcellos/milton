9ccb2403b9d6b7a9b50bc35514fd23e76d187318
Tomás de Siervi Barcellos <tomasbarcellos@gmail.com>
2022-02-06 11:56:36 -0300

    corrige grafia

--- tests/testthat/test-cep.R
+++ tests/testthat/test-cep.R
@@ -2,7 +2,7 @@ test_that("Retorna lat long de CEP", {
   expect_equal(get_addr(88063000), get_addr("Avenida pequeno principe"))
 
   expect_equal(milton:::cep(88063000),
-               "Avenida Pequeno Principe, Florianópolis")
+               "Avenida Pequeno Príncipe, Florianópolis")
   expect_equal(milton:::cep(70050000), "Esplanada Dos Ministérios, Brasília")
   expect_equal(milton:::cep(68915970), "Avenida Costa e Silva, Ferreira Gomes")
 
